f8295f9d88fc0a15593f5920611960533a928ec1
Emir Kartal <kartal.emirr@gmail.com>
2021-06-25 23:25:46 +0300

    Project setup

---
+++ NYT/NYT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NYT
+//
+//  Created by emir kartal on 25.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
