a131386f790023d55cb24a04040bf405fa846db2
Emir Kartal <kartal.emirr@gmail.com>
2021-06-27 22:13:00 +0300

    Set data to detail page

--- NYT/NYT/Modules/StoryDetail/StoryDetailViewController.swift
+++ NYT/NYT/Modules/StoryDetail/StoryDetailViewController.swift
@@ -21,12 +21,25 @@ final class StoryDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        configure()
+        
         viewModel.setStory()
     }
     
-    private func configure() {
+    private func configure(storyDetail: StoryResultModel) {
         self.title = "Story Detail"
+        let multimedia = storyDetail.multimedia?.first
+        detailImageView.setImageViewWithUrl(urlString: multimedia?.url ?? "")
+        titleLabel.text = storyDetail.title
+        descriptionLabel.text = storyDetail.abstract
+        authorLabel.text = storyDetail.byline
+        calculateConstraints(with: multimedia)
+    }
+    
+    private func calculateConstraints(with multimedia: MultimediaModel?) {
+        let height = CGFloat(multimedia?.height ?? 1)
+        let width = CGFloat(multimedia?.width ?? 1)
+        detailImageView.heightAnchor.constraint(equalTo: detailImageView.widthAnchor, multiplier: height/width).isActive = true
+        
     }
     
     @IBAction func seeMoreAct(_ sender: Any) {
@@ -38,7 +51,7 @@ extension StoryDetailViewController: StoryDetailViewModelDelegate {
     func handleStoryDetailViewModelOutput(_ output: StoryDetailViewModelOutput) {
         switch output {
         case .setStoryElements(let storyDetail):
-            
+            configure(storyDetail: storyDetail)
             break
         }
     }
